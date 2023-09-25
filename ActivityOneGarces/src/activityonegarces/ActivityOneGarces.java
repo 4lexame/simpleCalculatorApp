57c19916e5546f6f54aa67db7e7b3ef53f7de7bf
Delle <Delle@DESKTOP-N66RQ2L>
2023-09-25 20:06:02 +0800

    Commit - Garces

--- ActivityOneGarces/src/activityonegarces/ActivityOneGarces.java
+++ ActivityOneGarces/src/activityonegarces/ActivityOneGarces.java
@@ -15,6 +15,7 @@ public class ActivityOneGarces {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("Commit and Push -  Garces");
     }
     
 }
