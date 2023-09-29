7ca2a845511adb43c851611677a0d0e2376e4719
Delle <Delle@DESKTOP-N66RQ2L>
2023-09-29 17:08:31 +0800

    Initial Commit - Garces

---
+++ helloToGit/src/main/java/majgarces/hellotogit/HelloToGit.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package majgarces.hellotogit;
+
+/**
+ *
+ * @author Delle
+ */
+public class HelloToGit {
+
+    public static void main(String[] args) {
+        System.out.println("Hello to git!");
+    }
+}
