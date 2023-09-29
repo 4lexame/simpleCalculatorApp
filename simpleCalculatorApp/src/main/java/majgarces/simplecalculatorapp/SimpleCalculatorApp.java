297003328cffc9b14ff721671041c22f107e84ef
Delle <Delle@DESKTOP-N66RQ2L>
2023-09-29 17:54:38 +0800

    First commit- SimpleCalculatorApp - Garces

---
+++ simpleCalculatorApp/src/main/java/majgarces/simplecalculatorapp/SimpleCalculatorApp.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package majgarces.simplecalculatorapp;
+
+/**
+ *
+ * @author Delle
+ */
+public class SimpleCalculatorApp {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
